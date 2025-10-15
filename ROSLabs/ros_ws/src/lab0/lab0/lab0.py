42bfc1edd61566063350748e7980a66ee35ee019
cindygr <cindy.grimm@oregonstate.edu>
2025-10-15 14:54:38 -0700

    Go and Stop lab setup

--- ROSLabs/ros_ws/src/lab0/lab0/lab0.py
+++ ROSLabs/ros_ws/src/lab0/lab0/lab0.py
@@ -1,5 +1,7 @@
 def main():
     print('Hi from lab0.')
+    print('TO OBSERVE: Check out publisher & subscriber nodes (run both).')
+    print('TO OBSERVE: Run the circler node and display it in RViz2.')
 
 
 if __name__ == '__main__':
---
+++ ROSLabs/ros_ws/src/rob_stage/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+# We're going to use these to install the launch files.
+import os
+from glob import glob
+
+package_name = 'rob_stage'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # This line makes sure the launch files are installer.  This will copy
+        # all the files in the launch directory to the install location.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.xml'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cindygr',
+    maintainer_email='grimmc@oregonstate.edu',
+    description='Setup stage worlds for ROB456 and ROB 514',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
