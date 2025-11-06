dbf554a6732a56b8b5b7d1a6e66cfd6229353b48
Jamie Santos <jamiesanto@gmail.com>
2025-11-05 18:23:57 -0800

    Include environment files

--- mac_install.sh
+++ mac_install.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
 uv python install 3.12.7
 uv python pin 3.12.7
 uv init --bare
 uv add numpy scipy matplotlib otter-grader nbconvert ipykernel ipympl csv yaml skimage imageio 
 uv add pyqt5
+uv add numpy
+uv add scipy
+uv add matplotlib
+uv add otter-grader
+uv add matplotlib
+uv add nbconvert
+uv add ipykernel
+uv add ipympl
+uv add python-csv
+uv add pyyaml
+uv add scikit-image
+uv add imagio
